a90e773948b8598ef24c7b020890fc6c8ff00a97
ryo.a.murakami <ryo.a.murakami!@dena.jp>
2020-06-06 00:19:14 +0900

    firestore collection boilerplate

--- components/clientSide.tsx
+++ components/clientSide.tsx
@@ -7,16 +7,23 @@ type User = {
 
 export default function ClientSide() {
   const user = { id: "sample" };
-  const { data, update, error } = useDocument<User>(`users/${user.id}`, {
+  const { data, error } = useDocument<User>(`users/${user.id}`, {
     listen: true,
   });
 
+  const { data: collection, error: collectionError } = useCollection<User>(
+    "users"
+  );
+
   if (error) return <div>failed to load</div>;
   if (!data) return <div>Loading</div>;
+  if (collectionError) return <div>failed to load collection</div>;
+  if (!collection) return <div>Loading</div>;
   return (
     <>
       <h2>client side rendering</h2>
       <div>{data.name}</div>
+      <div>{collection[0].name}</div>
     </>
   );
 }
