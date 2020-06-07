2d24eb3dbcd4a7ecc11c03e7ba08da1e09e94c0c
ryo.a.murakami <ryo.a.murakami!@dena.jp>
2020-06-07 23:55:43 +0900

    very basic components done

--- components/BlogLink.tsx
+++ components/BlogLink.tsx
@@ -2,21 +2,26 @@ import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 import Date from "./date";
 import React from "react";
+import { FC } from "react";
 
-export function BlogLink(props: {
+type BlogLinkProps = {
   id: string;
   title: string;
   dateString: string;
-}) {
-  return (
-    <li className={utilStyles.listItem}>
-      <Link href={"/posts/[id]"} as={`/posts/${props.id}`}>
-        <a>{props.title}</a>
-      </Link>
-      <br />
-      <small className={utilStyles.lightText}>
-        <Date dateString={props.dateString} />
-      </small>
-    </li>
-  );
-}
+};
+
+export const BlogLink: FC<BlogLinkProps> = (props: {
+  id;
+  title;
+  dateString;
+}) => (
+  <li className={utilStyles.listItem}>
+    <Link href={"/posts/[id]"} as={`/posts/${props.id}`}>
+      <a>{props.title}</a>
+    </Link>
+    <br />
+    <small className={utilStyles.lightText}>
+      <Date dateString={props.dateString} />
+    </small>
+  </li>
+);
