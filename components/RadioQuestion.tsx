b6fb94cc29bed8c79b324cb80d51735b46decd22
Ryo Murakami <ryo.a.murakami@dena.com>
2020-08-02 00:36:20 +0900

    changed to material ui radio

--- components/RadioQuestion.tsx
+++ components/RadioQuestion.tsx
@@ -1,54 +1,57 @@
 import React, { FC } from "react";
 import { radioAnswerWithName, useUser } from "../src/atoms";
-import { useRecoilState } from "recoil";
 import { useSetRecoilState } from "recoil/dist";
 import { setUserProfile } from "../src/fetchers";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import { withStyles } from "@material-ui/core/styles";
+import { teal } from "@material-ui/core/colors";
+
+const TealRadio = withStyles({
+  root: {
+    color: teal[200],
+    "&$checked": {
+      color: teal[400],
+    },
+      padding: '0.25rem 0.5rem'
+  },
+  checked: {},
+})((props) => <Radio color="default" {...props} />);
 
 export type radioQuestionProps = {
   title: string;
   options: radioOption[];
 };
+
 export const RadioQuestion: FC<radioQuestionProps> = (props: {
   title;
   options;
 }) => {
-  return (
-    <>
-      {/*<div className="text-xl mb-2">*/}
-      {/*  <p>{props.title}</p>*/}
-      {/*</div>*/}
-      <div className="options">
-        {props.options.map((option, index) => (
-          <RadioOption {...option} key={index} />
-        ))}
-      </div>
-    </>
-  );
-};
-
-const RadioOption: FC<radioOption> = (props: { value; label; radioName }) => {
-  const [radioState, setRadioState] = useRecoilState(
-    radioAnswerWithName(props.radioName)
-  );
   const [user] = useUser();
+  const setRadioState = useSetRecoilState(
+    radioAnswerWithName(props.options[0].radioName)
+  );
   return (
-    <div className="radio_option">
-      <label>
-        <input
-          type="radio"
-          name={props.radioName}
-          value={props.value}
-          onChange={async (e: React.ChangeEvent<HTMLInputElement>) => {
-            setRadioState(e.target.value);
-            if (user)
-              await setUserProfile(user.uid, {
-                [props.radioName]: e.target.value,
-              });
-          }}
-          checked={props.value === radioState}
-        />
-        {props.label}
-      </label>
-    </div>
+    <FormControl component="fieldset">
+      <RadioGroup
+        onChange={async (e: React.ChangeEvent<HTMLInputElement>) => {
+          setRadioState(e.target.value);
+          if (user)
+            await setUserProfile(user.uid, {
+              [props.options[0].radioName]: e.target.value,
+            });
+        }}
+      >
+        {props.options.map((option) => (
+          <FormControlLabel
+            value={option.label}
+            label={option.label}
+            control={<TealRadio />}
+          />
+        ))}
+      </RadioGroup>
+    </FormControl>
   );
 };
